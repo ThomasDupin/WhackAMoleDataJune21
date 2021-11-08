8160c6d3810b961ed4a045008f5751da6f40f994
Thomas Dupin <thomas.dupin@viacesi.fr>
2021-11-08 10:20:29 +0100

    update analysis

--- whackamole_analysis.R
+++ whackamole_analysis.R
@@ -1,5 +1,19 @@
 library(plotly)
 library(tidyverse)
+library("devtools")
+install_github("tmalsburg/saccades/saccades", dependencies=TRUE, force=TRUE)
+library(saccades)
+library(rgl)
+library(reshape2)
+library(tidyverse)
+library("devtools")
+install_github("tidymodels/tidymodels")
+library(tidymodels)
+
+library(plotly)
+library(kernlab)
+library(pracma) #For meshgrid()²
+library(lubridate)
 options("digits.secs"=6)
 
 fig <- plot_ly() %>%
@@ -23,13 +37,20 @@ nbPlayer = D %>%
 #############
 #  2) For how long time did each participant play?
 #############
- TimeSpend = D %>%
-    select(Participant, GameTimeSpent) %>%
-    filter(GameTimeSpent!="NA") %>%
-    group_by(Participant)%>%
-    summarise(sum(as.integer(GameTimeSpent))) %>%
-    rename(GameTimeSpentS = "sum(as.integer(GameTimeSpent))")%>%
-    mutate(GameTimeSpentH=GameTimeSpentS/3600 )
+
+SessionStart = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Started") 
+
+SessionEnd = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Finished") 
+
+PlayTime = merge(SessionStart,SessionEnd,"SessionID") %>%
+  mutate(Duration = difftime(Timestamp.y,Timestamp.x )) %>%
+  group_by(Participant.x)%>%
+  summarise(timePlayed=sum(Duration))%>%
+  rename(Participant = "Participant.x")
   
 #############
 # 3) What were the participants’ mean world gaze position?
@@ -58,11 +79,441 @@ nbPlayer = D %>%
 # 5) Please take a look at the “saccades” package for R and investigate how we can use it for the Whack-A-Mole eye tracking data.
 #############
 
+#############
+# A) How high frequency is the eye tracker running at? Hz = Samples per second.
+#############
+
+############
+# Change the time value, from 0 sec at the first event to 2min at the last
+###########
+
+SessionTimeChange = D %>%
+  select(Participant, SessionID, Event, Timestamp, Condition) %>%
+  filter(SessionID=="7012cac88a9dab2d7acee4480cd1014e")
+
+
+
+SessionStart = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Started",SessionID=="7012cac88a9dab2d7acee4480cd1014e" ) 
+
+
+SessionEnd = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Finished",SessionID=="7012cac88a9dab2d7acee4480cd1014e") 
+
+
+
+DateRange <- SessionTimeChange[SessionTimeChange$Timestamp > '2021-06-29 13:46:56.7651' & SessionTimeChange$Timestamp < '2021-06-29 13:48:58.6758', ]
+
+DataHz = DateRange %>%
+  summarise(Participant,
+            SessionID,
+            Event,
+            Timestamp = difftime( Timestamp, SessionStart$Timestamp),
+            Condition)%>%
+  mutate(time= format(Timestamp, format = "%M:%S"))
+
+
+
+# Use time stamp to calculate how long a test took
+
+DateRange$Timestamp <- as.POSIXlt(DateRange$Timestamp, format = "%m/%d/%Y %H:%M:%S:%OS")
+
+
+
+
+# convert the timer into time in seconds
+DataHz$time <- format(DateRange$Timestamp, format = "%H:%M:%S:%OS")
+
+DataHz$time <- round(DateRange$Timestamp$sec, 0) + DateRange$Timestamp$min * 60 + DateRange$Timestamp$hour * 3660
+
+DateHz = DataHz %>% 
+  filter(Event == "Game Started")%>% 
+    select(SessionID, Timestamp) %>% rename(Timestamp_start = Timestamp) %>%
+    right_join(DataHz) %>%
+    mutate(time_since_start = time - Timestamp_start)
+ 
+
+
+############
+# calculate the HZ
+###########
+
+SampleHzSession = D %>%
+  select(Participant, SessionID, Event,Condition, Timestamp) %>%
+    filter(Event =="Sample")%>%
+    mutate(minute= format(Timestamp, format = "%M:%S"))%>%
+    group_by(SessionID, minute)%>%
+    summarise(
+              Participant= Participant,
+              Hz_Rate = sum(Event=="Sample"),
+              Condition = Condition
+              )%>%
+  group_by(SessionID) %>%
+  summarise(Hz= mean(Hz_Rate),
+            Participant= Participant,
+            Condition= Condition)
+
+
+SampleHz <- unique(SampleHzSession)
+
+###############
+#Below for one session
+##################
+ 
+SampleHzOneSession = D %>%
+  select(Participant, SessionID, Event,Condition, Timestamp) %>%
+  filter(Event =="Sample", SessionID=="057864beeebb3ecb4d1cb631914c376a")%>%
+  mutate(minute= format(Timestamp, format = "%M:%S"))%>%
+  group_by(minute)%>%
+  summarise(
+    Participant= Participant,
+    Hz_Rate = sum(Event=="Sample")
+  )
+  
+
+SampleHzOneSessionUnique <- unique(SampleHzOneSession)
+
+
+############
+# Graph with Hz over time for a participant
+###########
+
+
+DisplayHz <- SampleHzOneSessionUnique 
+
+
+# One trace (more performant, but less interactive)
+DisplayHz %>%
+  plot_ly(x = ~minute(m), y = ~Hz_Rate, type = "scatter", mode="line")
+  
+
+# Multiple traces (less performant, but more interactive)
+plot_ly(DisplayHz, x = ~minute, y = ~Hz_Rate) %>%
+  add_lines(color = ~ordered(SessionID))
+
+# The split argument guarantees one trace per group level (regardless 
+# of the variable type). This is useful if you want a consistent
+# visual property over multiple traces 
+# plot_ly(econ, x = ~mnth, y = ~uempmed) %>%
+#   add_lines(split = ~yr, color = I("black"))
+
+
+
+
+
+#########################
+# NOT WORKING AT ALL
+########################
+
+
+SessionTimeChange = D %>%
+  select(Participant, SessionID, Event, Timestamp, Condition) %>%
+  filter(SessionID=="7012cac88a9dab2d7acee4480cd1014e")
+
+
+
+SessionStart = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Started",SessionID=="7012cac88a9dab2d7acee4480cd1014e" ) 
+
+
+SessionEnd = D %>%
+  select(Participant, SessionID, Event, Timestamp) %>%
+  filter(Event =="Game Finished",SessionID=="7012cac88a9dab2d7acee4480cd1014e") 
+
+
+
+DateRange <- SessionTimeChange[SessionTimeChange$Timestamp > '2021-06-29 13:46:56.7651' & SessionTimeChange$Timestamp < '2021-06-29 13:48:58.6758', ]
+
+DataHz = DateRange %>%
+  summarise(Participant,
+            SessionID,
+            Event,
+            Timestamp = difftime( Timestamp, SessionStart$Timestamp),
+            Condition)%>%
+  mutate(time= format(Timestamp, format = "%S"))
+
+
+SessionHz <- DataHz%>%
+  mutate(minute= format(time, format = "%M:%S"))%>%
+  group_by(minute)%>%
+  summarise(
+    Hz_Rate = sum(Event=="Sample")
+  )
+
+
+
+SessionHz <- D%>%
+  mutate(minute= format(Timestamp, format = "%M:%S"))%>%
+  group_by(minute)%>%
+  summarise(
+    Hz_Rate = sum(Event=="Sample")
+  )
+
+
+#  filter(Event =="Sample", SessionID=="057864beeebb3ecb4d1cb631914c376a")%>%
+
+SessionHzUnique <- unique(SessionHz)
+
+#########################
+# NOT WORKING AT ALL
+########################
+
+ 
+
+############
+# B) how many fixations on left/right side of whack-a-mole wall?
+#############
+
+
+################
+# This is to get the data of a particular session or a participant
+
+GazeDirection = D %>%
+  select(Participant,contains("WorldGazeHitPosition"),SessionID)%>%
+  filter(WorldGazeHitPositionX!="NA")%>%
+  filter(SessionID=="55aa012d4ac2315806e052fa911c9343")%>%
+  summarise(GazeX= as.numeric(WorldGazeHitPositionX),
+            GazeY= as.numeric(WorldGazeHitPositionY),
+            GazeZ= as.numeric(WorldGazeHitPositionZ))
+  
+
+GazeDirection = D %>%
+  select(Participant,contains("WorldGazeHitPosition"),SessionID)%>%
+  filter(WorldGazeHitPositionX!="NA")%>%
+  filter(Participant =='1')
+
+########################################
+
+
+
+###############
+# Draw a point with the coordonate of the gaze, this work for 1 session not the whole data of a participant.
+#############
+
+data <- GazeDirection
+  
+  plot3d( 
+    x=data$`GazeX`, y=data$`GazeY`, z=data$`GazeZ`, 
+    type = 's', 
+    radius = .01,
+    xlab="GazeX", ylab="GazeY", zlab="GazeZ")
+
+
+  ###############
+  # Draw a point with the coordonate of the gaze, not like above its one participant but the data throught all his sessions
+  ############# 
+  
+  GazeDirectionWhole = D %>%
+    select(Participant,contains("WorldGazeHitPosition"),SessionID)%>%
+    filter(WorldGazeHitPositionX!="NA", Participant=="1")%>%
+    group_by(Participant)%>%
+    summarise(GazeX= WorldGazeHitPositionX,
+              GazeY= WorldGazeHitPositionY,
+              GazeZ= WorldGazeHitPositionZ)
+    
+
+  data <- GazeDirectionWhole  
+  
+  plot3d( 
+    x=data$`GazeX`, y=data$`GazeY`, z=data$`GazeZ`, 
+    type = 's', 
+    radius = .01,
+    xlab="GazeX", ylab="GazeY", zlab="GazeZ")
+  
+
+
+####################
+# Here this is for the heatmap data for the eye tracking, during one session
+###################
+
+
+data <- GazeDirection
+
+
+mesh_size <- .02
+margin <- 0
+X <- data %>% select(GazeX, GazeY)
+y <- data %>% select(GazeZ)
+
+model <- svm_rbf(cost = 1.0) %>% 
+  set_engine("kernlab") %>% 
+  set_mode("regression") %>% 
+  fit(GazeZ ~ GazeX + GazeY, data = data)
+
+x_min <- min(X$GazeX) - margin
+x_max <- max(X$GazeX) - margin
+y_min <- min(X$GazeY) - margin
+y_max <- max(X$GazeY) - margin
+xrange <- seq(x_min, x_max, mesh_size)
+yrange <- seq(y_min, y_max, mesh_size)
+xy <- meshgrid(x = xrange, y = yrange)
+xx <- xy$X
+yy <- xy$Y
+dim_val <- dim(xx)
+xx1 <- matrix(xx, length(xx), 1)
+yy1 <- matrix(yy, length(yy), 1)
+final <- cbind(xx1, yy1)
+pred <- model %>%
+  predict(final)
+
+pred <- pred$.pred
+pred <- matrix(pred, dim_val[1], dim_val[2])
+
+fig <- plot_ly(data, x = ~GazeX, y = ~GazeY, z = ~GazeZ ) %>% 
+  add_markers(size = 5) %>% 
+  add_surface(x=xrange, y=yrange, z=pred, alpha = 0.65, type = 'mesh3d', name = 'pred_surface')
+fig
+
+################
+# try using the saccade package to find some fixation
+#
+#This work for only one session 
+###############
+
+
+GameStartTimestamp = D %>%
+  select(SessionID, Timestamp,Event) %>%
+  filter(Event =="Game Started",SessionID=="55aa012d4ac2315806e052fa911c9343" ) 
+
+
+
+fixationSession = D %>%
+  select(Participant,contains("WorldGazeHitPosition"), SessionID, Event, Timestamp) %>%
+  filter(SessionID=="55aa012d4ac2315806e052fa911c9343")
+
+
+fixationSessionRange <- fixationSession[fixationSession$Timestamp > '2021-06-28 13:50:30.06' & fixationSession$Timestamp < '2021-06-28 13:52:31.9802', ]
+
+GazeDataSession = fixationSessionRange %>%
+  summarise(x= as.numeric(WorldGazeHitPositionX),
+            y= as.numeric(WorldGazeHitPositionY),
+            trial= SessionID,
+            time = difftime( Timestamp, GameStartTimestamp$Timestamp)
+            ) %>%
+  filter(x!="NA",y!="NA")
  
   
+
+fixations <- subset(detect.fixations(GazeDataSession), event=="fixation") 
   
+head(fixations)
+
+##################
+# Display the fixations
+#################
+ggplot(fixations, aes(x, y)) +
+  geom_point(size=0.7) +
+  coord_fixed() +
+  facet_wrap(~trial)
+
+
+###########
+# Fixation duration over time 
+##########
+
+fixations %>%
+  plot_ly(x = ~start, y = ~dur, type = 'scatter', mode = 'lines') 
   
+
+############
+#This works for one participant at the time
+############
+GazeDataParticipant= D%>%
+  select(Participant,contains("WorldGazeHitPosition"),Timestamp)%>%
+  filter(WorldGazeHitPositionX!="NA")%>%
+  filter(Participant=="14")%>%
+  summarise(time = Timestamp,
+            x= as.numeric(WorldGazeHitPositionX),
+            y= as.numeric(WorldGazeHitPositionY),
+            trial= Participant)
+
+fixationsParticipant <- subset(detect.fixations(GazeDataParticipant), event=="fixation")
+head(fixationsParticipant)
+
+
+ggplot(fixationsParticipant, aes(x, y)) +
+  geom_point(size=0.7) +
+  coord_fixed() +
+  facet_wrap(~trial)
+
+
+############
+#This works summarise everything
+############
+GazeData= D%>%
+  select(Participant,contains("WorldGazeHitPosition"),Timestamp)%>%
+  filter(WorldGazeHitPositionX!="NA", Participant!="3")%>%
+  summarise(time = Timestamp,
+            x= as.numeric(WorldGazeHitPositionX),
+            y= as.numeric(WorldGazeHitPositionY),
+            trial= Participant)
+ 
+
+
+fixations <- subset(detect.fixations(GazeData), event=="fixation")
+head(fixations)
+
+
+
+ggplot(fixations, aes(x, y)) +
+  geom_point(size=0.7) +
+  coord_fixed() +
+  facet_wrap(~trial)
+
+
+############
+# C) how long fixation durations on the left/right side of the whack-a-mole wall?
+#############
+
+
+
+
+
+############
+# D) Investigate how to calculate “saccade peak velocity”.
+#############
+#####
+# For the saccade peak velocity when can re use the datatable fixations from the the question B. There is a peak vx and vy
   
-  
-  
-  
+GameStartTimestamp = D %>%
+  select(SessionID, Timestamp,Event) %>%
+  filter(Event =="Game Started",SessionID=="55aa012d4ac2315806e052fa911c9343" ) 
+
+
+
+fixationSession = D %>%
+  select(Participant,contains("WorldGazeHitPosition"), SessionID, Event, Timestamp) %>%
+  filter(SessionID=="55aa012d4ac2315806e052fa911c9343")
+
+
+fixationSessionRange <- fixationSession[fixationSession$Timestamp > '2021-06-28 13:50:30.06' & fixationSession$Timestamp < '2021-06-28 13:52:31.9802', ]
+
+GazeStat = fixationSessionRange %>%
+  summarise(x= as.numeric(WorldGazeHitPositionX),
+            y= as.numeric(WorldGazeHitPositionY),
+            trial= SessionID,
+            time = difftime( Timestamp, GameStartTimestamp$Timestamp)
+  ) %>%
+  filter(x!="NA",y!="NA")
+
+
+
+DataFixations <- subset(detect.fixations(GazeStat), event=="fixation") 
+
+diagnostic.plot(GazeStat, DataFixations)
+
+#########
+# Some Stats with peak velocity on X and on Y
+########
+statsVelocity <- calculate.summary(DataFixations)
+round(statsVelocity, digits=2)
+
+
+peakVelocity <- c(DataFixations$peak.vx,DataFixations$peak.vy)
+length(peakVelocity)
+
+
+
